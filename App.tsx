8258d5c498f9d6b2af58f1a19e10cc4130fb6224
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-12-08 20:49:02 -0300

    new animated menu button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable ,Button, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import { Pressable ,Button, StyleSheet, Text, Touchable, TouchableOpacity, View, Animated } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { FontAwesome5 } from '@expo/vector-icons';
 import Ionicons from '@expo/vector-icons/Ionicons';
@@ -9,39 +9,117 @@ import WaterBlock from './components/WaterBlock';
 import SettingsBlock from './components/SettingsBlock';
 
 export default function App() {
+  const [pop, setPop] = useState<boolean>(false);
 
-  const [color, setColor] = useState<string>('#fff');
-  const [color1, setColor1] = useState<string>('#fff');
-  const [color2, setColor2] = useState<string>('#fff');
-  const [color3, setColor3] = useState<string>('#fff');
+  const [icon_1] = useState(new Animated.Value(40));
+  const [icon_2] = useState(new Animated.Value(40));
+  const [icon_3] = useState(new Animated.Value(40));
+  const [icon_4] = useState(new Animated.Value(40));
+
+  const popIn = () => {
+    if (pop === false) {
+      setIconMenu('close');
+    }else{
+      setIconMenu('menu');
+    }
+    setPop(true);
+    Animated.timing(icon_1, {
+      toValue: 155,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_2, {
+      toValue: 97,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_3, {
+      toValue: 155,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_4, {
+      toValue: 155,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  const popOut = () => {
+    if (pop === true) {
+      setIconMenu('menu');
+    }else{
+      setIconMenu('close');
+    }
+    setPop(false);
+    Animated.timing(icon_1, {
+      toValue: 40,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_2, {
+      toValue: 40,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_3, {
+      toValue: 40,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+    Animated.timing(icon_4, {
+      toValue: 40,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  const [iconMenu, setIconMenu] = useState<'menu' | 'close'>('menu');
 
   const [preVisibility, setPreVisibility] = useState<string>('none');
   const [preVisibilit1, setPreVisibility1] = useState<string>('none');
   const [preVisibility2, setPreVisibility2] = useState<string>('none');
   const [preVisibility3, setPreVisibility3] = useState<string>('none');
 
+  
 
   return (
     <View style={styles.container}>
 
-      <SettingsBlock visibility={preVisibility}/>
+      <Animated.View style={[styles.circle, { bottom: icon_1}]}>
+        <TouchableOpacity>
+          <Ionicons name="water" size={25} color="#FFFF" />
+        </TouchableOpacity>
+      </Animated.View>
 
-      <LinearGradient start={{x: 0.2, y: 0.1}} colors={['#606673', '#434957']}  style={styles.header}>
-        <View style={styles.optionsBlock}>
-          <Pressable onPress={() => {setPreVisibility('flex'); setPreVisibility1('none'); setPreVisibility2('none'); setPreVisibility3('none') ;setColor('#4691E8'); setColor1('#fff'); setColor2('#fff'); setColor3('#fff')}}  style={[styles.headerOptions]}>
-            <Ionicons name="settings-outline" size={32} color={color} />
-          </Pressable>
-          <Pressable onPress={() => {setPreVisibility1('flex'); setPreVisibility('none'); setPreVisibility2('none'); setPreVisibility3('none') ;setColor('#fff'); setColor1('#4691E8'); setColor2('#fff'); setColor3('#fff')}}  style={[styles.headerOptions]}>
-            <Ionicons name="barbell-outline" size={32} color={color1} />
-          </Pressable>
-          <Pressable onPress={() => {setPreVisibility2('flex'); setPreVisibility1('none'); setPreVisibility3('none'); setPreVisibility('none') ;setColor('#fff'); setColor1('#fff'); setColor2('#4691E8'); setColor3('#fff')}} style={[styles.headerOptions]}>
-            <Ionicons name="water-outline" size={32} color={color2} />
-          </Pressable>
-          <Pressable onPress={() => {setPreVisibility3('flex'); setPreVisibility1('none'); setPreVisibility2('none'); setPreVisibility('none') ;setColor('#fff'); setColor1('#fff'); setColor2('#fff'); setColor3('#4691E8')}}  style={[styles.headerOptions]}>
-            <Ionicons name="basketball-outline" size={32} color={color3} />
-          </Pressable>
-        </View>
-      </LinearGradient>
+      <Animated.View style={[styles.circle, { bottom: icon_2, right: icon_2}]}>
+        <TouchableOpacity>
+          <Ionicons name="bicycle" size={25} color="#FFFF" />
+        </TouchableOpacity>
+      </Animated.View>
+
+      <Animated.View style={[styles.circle, { bottom: icon_3, right: icon_3}]}>
+        <TouchableOpacity>
+          <Ionicons name="barbell" size={25} color="#FFFF" />
+        </TouchableOpacity>
+      </Animated.View>
+
+      <Animated.View style={[styles.circle, { right: icon_4}]}>
+        <TouchableOpacity>
+          <Ionicons name="settings" size={25} color="#FFFF" />
+        </TouchableOpacity>
+      </Animated.View>
+
+      <TouchableOpacity
+        style={styles.circle}
+        onPress={() => {
+          pop === false ? popIn() : popOut();
+        }}
+      >
+        <Ionicons name={iconMenu} size={35} color="#FFFF" />
+      </TouchableOpacity>
+
+      
     </View>
   );
 }
@@ -82,5 +160,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '100%',
     height: 60,
-  }
+  },
+  circle: {
+    backgroundColor: '#f52d56',
+    width: 60,
+    height: 60,
+    position: 'absolute',
+    bottom: 40,
+    right: 40,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+ }
 });
